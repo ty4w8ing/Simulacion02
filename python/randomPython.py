1f647e445df312036244af4b2da4a43b59ce4ada
TY4W8ING <tavog14@gmail.com>
2016-08-21 14:19:08 -0600

    python random

---
+++ python/randomPython.py
@@ -0,0 +1,15 @@
+import random
+
+
+def generar_random():
+	return random.random()
+
+def escribir_en_archivo():
+	file = open("python.txt", "w")
+	for i in range(0,1000000):
+		numero = "%.50f" % generar_random()
+		file.write(numero+'\n')
+	file.close()
+
+if __name__ == '__main__':
+	escribir_en_archivo()
