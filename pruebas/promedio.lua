80bdeb7ddfc9f8971990b4dd801d4d3b1d7e5a6a
José Gustavo Gonzalez González <tavog14@gmail.com>
2016-09-09 20:38:49 -0600

    Delete promedio.lua

--- pruebas/promedio.lua
+++
@@ -1,8 +0,0 @@
-function fsize (file)
-    local current = file:seek()      -- get current position
-    local size = file:seek("end")    -- get file size
-    file:seek("set", current)        -- restore position
-	return size
-end
-
-fsize("promedio.lua")
